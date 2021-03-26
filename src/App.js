45532c6163b9c90b30f7a6faf6f80e59b5895c01
Omar Asamar <asamaromar@gmail.com>
2021-03-26 08:06:18 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+ import './App.css';
+
+
+ // files import
+ import 'bootstrap/dist/css/bootstrap.min.css';
+
+// main function
+function App() {
+  return (
+
+
+    <div className="text-center">
+      qsqs
+    </div>
+
+
+  );
+}
+
+export default App;
